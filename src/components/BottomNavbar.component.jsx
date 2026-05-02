3f22df598b61a3a7299d0ea726f2734006b0c29f
Tanuj Khedar <tanujkhedar@gmail.com>
2026-05-02 17:32:39 +0530

    bottom navbar active bihaviur implimented

--- src/components/BottomNavbar.component.jsx
+++ src/components/BottomNavbar.component.jsx
@@ -1,13 +1,32 @@
 import { Home, MessageCircleMore, Upload, User, Video } from 'lucide-react'
+import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
+
 
 const BottomNavbar = () => {
+  const [isHomeActive, setHomeActive] = useState(false);
+  const [isReelsActive, setReelsActive] = useState(false);
+  const [isUploadActive, setUploadActive] = useState(false);
+  const [isMessageActive, setMessageActive] = useState(false);
+  const [isProfileActive, setProfileActive] = useState(false);
+
   return (
-    <div className='flex justify-around p-4 border-t border-gray-300 bg-white shadow'>
-        <button><Home/></button>
-        <button><Video/></button>
-        <button><Upload/></button>
-        <button><MessageCircleMore/></button>
-        <button><User/></button>
+    <div className='flex justify-around p-1 border-t border-gray-300 bg-white shadow'>
+        <NavLink to='/' end className={({isActive}) => { setHomeActive(isActive); return `p-3 rounded-full  ${isActive ? 'bg-purple-200' : ' '}`}} >
+          <Home color={isHomeActive ? "Purple" : " black"}/>
+        </NavLink>
+        <NavLink to='/reels' end className={({isActive}) => { setReelsActive(isActive); return `p-3 rounded-full  ${isActive ? 'bg-purple-200' : ' '}`}}>
+          <Video color={isReelsActive ? "Purple" : " black"}/>
+        </NavLink>
+        <NavLink to='/upload' end className={({isActive}) => { setUploadActive(isActive); return `p-3 rounded-full  ${isActive ? 'bg-purple-200' : ' '}`}}>
+          <Upload color={isUploadActive ? "Purple" : " black"}/>
+        </NavLink>
+        <NavLink to='/message' end className={({isActive}) => { setMessageActive(isActive); return `p-3 rounded-full  ${isActive ? 'bg-purple-200' : ' '}`}}>
+          <MessageCircleMore color={isMessageActive ? "Purple" : " black"}/>
+        </NavLink>
+        <NavLink to='/profile' end className={({isActive}) => { setProfileActive(isActive); return `p-3 rounded-full  ${isActive ? 'bg-purple-200' : ' '}`}} >
+          <User color={isProfileActive ? "Purple" : " black"}/>
+        </NavLink>
     </div>
   )
 }
--- src/components/StoryHighlight.component.jsx
+++ src/components/StoryHighlight.component.jsx
@@ -1,5 +1,5 @@
-const storyData = <div className=' mr-4 shrink-0'>
-            <img className='w-15 rounded-full border-3 p-0.5 m-0.5' src="https://img.magnific.com/premium-photo/design-professional-profile-picture-with-sharp-focus-natural-lighting-clean-neutral-background_880763-20280.jpg?w=1060" alt="story" />
+const storyData = <div className='shrink-0'>
+            <img className='w-15 rounded-full border-3 border-purple-600 p-0.5 m-0.5' src="https://img.magnific.com/premium-photo/design-professional-profile-picture-with-sharp-focus-natural-lighting-clean-neutral-background_880763-20280.jpg?w=1060" alt="story" />
             <div className='text-center text-gray-500 font-medium'>Tanuj</div>
         </div>
 
@@ -13,7 +13,7 @@ while(i<10) {
 
 const StoryHighlight = () => {
   return (
-    <div className='flex overflow-x-auto bg-white shadow p-4'>
+    <div className='flex overflow-x-auto bg-white shadow py-4 px-6 gap-10'>
         {stories.map((item, i) => item)}
     </div>
   )
